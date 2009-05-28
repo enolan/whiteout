679118b1613bbed806423747ab65c8f928bf3314
Echo Nolan <echo@echonolan.net>
2009-05-28 16:28:47 -0700

    Refactor verifySingleFile

--- src/Test/Network/Whiteout.hs
+++ src/Test/Network/Whiteout.hs
@@ -1,5 +1,6 @@
 module Test.Network.Whiteout where
 
+import Control.Applicative
 import Control.Concurrent.STM
 import Data.Array.IArray
 import Data.Digest.SHA1
@@ -9,29 +10,31 @@ import Test.HUnit
 
 import Network.Whiteout
 
+loadTorWithAssert :: FilePath -> IO Torrent
+loadTorWithAssert path = do
+    tor <- loadTorrentFromFile path
+    assertBool "Torrent didn't load" $ isJust tor
+    return $ fromJust tor
+
+addTorWithAssert :: Session -> Torrent -> FilePath -> IO TorrentSt
+addTorWithAssert sess tor path = do
+    res <- addTorrent sess tor path
+    assertBool "adding failed" res
+    torst <- M.lookup (infohash tor) <$> (atomically $ getActiveTorrents sess)
+    assertBool "Couldn't find TorrentSt" $ isJust torst
+    return $ fromJust torst
+
 verifySingleFile :: Assertion
 verifySingleFile = do
     sess <- initialize
-    tor <- loadTorrentFromFile
-        "test-data/01_-_Brad_Sucks_-_Dropping_out_of_School.mp3.torrent"
-    assertBool "Torrent didn't load" $ isJust tor
-    let tor' = fromJust tor
-    res <- addTorrent sess tor'
-        "test-data/01_-_Brad_Sucks_-_Dropping_out_of_School.mp3"
-    assertBool "Adding failed" res
-    torrents <- atomically $ getActiveTorrents sess
-    let
-        torst = M.lookup
-            (Word160 958421318 1479285429 3749561784 3909306547 1811215688)
-            torrents
-    assertBool "Couldn't find TorrentSt" $ isJust torst
-    let torst' = fromJust torst
-    beginVerifyingTorrent torst'
+    tor <- loadTorWithAssert "test-data/01_-_Brad_Sucks_-_Dropping_out_of_School.mp3.torrent"
+    torst <- addTorWithAssert sess tor "test-data/01_-_Brad_Sucks_-_Dropping_out_of_School.mp3"
+    beginVerifyingTorrent torst
     atomically $ do
-        activity <- getActivity torst'
+        activity <- getActivity torst
         if activity == Verifying
             then retry
             else return ()
-    let (_, max) = bounds $ pieceHashes tor'
-    allGood <- fmap and $ atomically (mapM (isPieceComplete torst') [0..max])
+    let (_, max) = bounds $ pieceHashes tor
+    allGood <- fmap and $ atomically (mapM (isPieceComplete torst) [0..max])
     assertBool "Not all pieces passed verification" allGood
