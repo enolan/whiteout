93b8a26df76b519183ef510d4a7493967dc03dc9
Echo Nolan <echo@echonolan.net>
2009-06-01 04:22:01 -0700

    Implement reading pieces from multi-file torrents

--- src/Internal/Pieces.hs
+++ src/Internal/Pieces.hs
@@ -7,6 +7,7 @@ import Control.Applicative
 import qualified Control.Exception as C
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
+import System.FilePath ((</>))
 import System.IO
 
 import Internal.Types
@@ -27,5 +28,29 @@ getPiece torst piecenum = let
                             (\h -> do
                                 hSeek h AbsoluteSeek offset
                                 Just <$> B.hGet h (pieceLen tor))
-                Right _ -> return Nothing)
+                Right filelist ->
+                    getPieceMultiFile
+                        (pieceLen tor) (path torst) filelist offset)
             (\(_ :: C.IOException) -> return Nothing)
+
+-- If this becomes a performance concern we could use an interval tree.
+getPieceMultiFile ::
+    Int -> FilePath -> [(Integer, FilePath)] -> Integer ->
+    IO (Maybe ByteString)
+getPieceMultiFile pieceSize prefix files offset =
+    fmap (fmap B.concat) $ go files offset pieceSize
+    where
+    go :: [(Integer, FilePath)] -> Integer -> Int -> IO (Maybe [ByteString])
+    go []               _      neededBytes = if neededBytes > pieceSize
+        then return Nothing
+        else return $ Just []
+    go _                _      0           = return $ Just []
+    go ((len, path):fs) offset neededBytes = if offset > len
+        then go fs (offset-len) neededBytes
+        else do
+            chunk <- withBinaryFile (prefix </> path) ReadMode
+                (\h -> do
+                    hSeek h AbsoluteSeek offset
+                    B.hGet h neededBytes)
+            rest <- go fs 0 (neededBytes - (B.length chunk))
+            return $ (chunk:) <$> rest
