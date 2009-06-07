d08573b8865ecbb679de674a2b2a56397ea9c496
Echo Nolan <echo@echonolan.net>
2009-06-07 14:59:37 -0700

    Improve error handling a bit in addPeer; fix a latent bug

--- src/Internal/Peer.hs
+++ src/Internal/Peer.hs
@@ -5,11 +5,13 @@ module Internal.Peer
 
 import Control.Applicative
 import Control.Concurrent (forkIO)
+import Control.Exception
 import Data.Array.IArray (bounds)
 import Data.Binary
 import Data.Binary.Put
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as L
+import Data.Int (Int64)
 import Data.Maybe (fromJust)
 import Network.Socket
 import qualified Network.Socket.ByteString as SB
@@ -23,14 +25,13 @@ import Internal.Types
 -- thread should empty the queue with a limit on the max open/half-open
 -- connections.
 addPeer :: Session -> TorrentSt -> String -> PortNumber -> IO ()
-addPeer sess torst h p = (forkIO go) >> return ()
+addPeer sess torst h p = (forkIO $ catches go handlers) >> return ()
     where
-        go = do
-            s <- socket AF_INET Stream 0
+        go = bracket (socket AF_INET Stream 0) (sClose) $ \s-> do
             addr <- inet_addr h
             connect s $ SockAddrInet p addr
             sendHandshake sess torst s
-            theirHandshake :: Handshake <- decode <$> SBL.recv s 68
+            theirHandshake :: Handshake <- decode <$> recvAll s 68
             print theirHandshake
             let
                 numPieces = snd $ bounds $ pieceHashes $ torrent $ torst
@@ -39,24 +40,30 @@ addPeer sess torst h p = (forkIO go) >> return ()
             sendPeerMsg s $ Bitfield $ B.replicate bitFieldLen 255
             sendPeerMsg s $ Unchoke
             peerHandler torst s
+        handlers = [
+            Handler (\(e :: IOException) -> print e),
+            Handler (\(e :: ErrorCall) -> print e)
+            ]
 
 -- PeerMsg handling loop
 peerHandler :: TorrentSt -> Socket -> IO ()
 peerHandler torst s = do
-    msgLen :: Word32 <- decode <$> SBL.recv s 4
+    msgLen :: Word32 <- decode <$> recvAll s 4
     if msgLen > 0
-        then do msg <- decode <$> SBL.recv s (fromIntegral msgLen)
-                case msg of
-                    it@(Request pn offset len) -> do
-                        print it
-                        dataToSend <-
-                            (B.take (fromIntegral len) .
-                                B.drop (fromIntegral offset)) <$>
-                            fromJust <$> getPiece torst pn
-                        let msgToSend = Piece pn offset dataToSend
-                        sendPeerMsg s msgToSend >> peerHandler torst s
-                    other -> print other >> peerHandler torst s
-        else peerHandler torst s
+        then do
+            msg <- decode <$> recvAll s (fromIntegral msgLen)
+            case msg of
+                it@(Request pn offset len) -> do
+                    print it
+                    dataToSend <-
+                        (B.take (fromIntegral len) .
+                            B.drop (fromIntegral offset)) <$>
+                        fromJust <$> getPiece torst pn
+                    let msgToSend = Piece pn offset dataToSend
+                    sendPeerMsg s msgToSend
+                other -> print other
+        else return () -- Keepalive
+    peerHandler torst s
 
 sendHandshake :: Session -> TorrentSt -> Socket -> IO ()
 sendHandshake sess torst s = SBL.sendAll s $ encode Handshake {
@@ -70,3 +77,16 @@ sendPeerMsg s p = SBL.sendAll s $ runPut $ do
     let p' = encode p
     put ((fromIntegral $ L.length p') :: Word32)
     putLazyByteString p'
+
+-- Get the specified number of bytes from a socket, retrying until done.
+recvAll :: Socket -> Int64 -> IO L.ByteString
+recvAll s numBytes = do
+    whatWeGot <- SBL.recv s numBytes
+    let count = L.length whatWeGot
+    case False of
+        _ | count == 0 -> error "recvAll: couldn't get sufficient bytes"
+          | count < numBytes -> do
+                rest <- recvAll s (numBytes - (fromIntegral count))
+                return $ L.append whatWeGot rest
+          | count == numBytes -> return whatWeGot
+          | otherwise -> error "recvAll: the impossible happened"
