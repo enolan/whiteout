1eb8df3b9e01a452c92538ee0db6e97291f13cbb
Echo Nolan <echo@echonolan.net>
2009-10-15 18:03:57 -0700

    Timestamp log messages.

--- src/Internal/Logging.hs
+++ src/Internal/Logging.hs
@@ -1,9 +1,12 @@
 module Internal.Logging where
 
+import Control.Applicative
 import Control.Concurrent (forkIO)
 import Control.Concurrent.STM
 import Control.Monad (forever)
+import Data.Time (formatTime, getZonedTime)
 import System.IO
+import System.Locale (defaultTimeLocale)
 
 import Internal.Types
 
@@ -15,7 +18,8 @@ maybeLog sess level msg = case logChan sess of
 -- | Directs the passed logging channel to console.
 logToConsole :: TChan (LogLevel, String) -> IO ()
 logToConsole chan = do
-    forkIO $ forever $ atomically (readTChan chan) >>= print
+    forkIO $ forever $
+        atomically (readTChan chan) >>= uncurry genLogMsg >>= putStrLn
     return ()
 
 -- | Directs the passed logging channel to a given file. Overwrites the content
@@ -25,5 +29,10 @@ logToFile path chan = do
     forkIO $ withFile path WriteMode $ \h -> do
         hSetBuffering h LineBuffering
         forever $
-            atomically (readTChan chan) >>= hPrint h
+            atomically (readTChan chan) >>= uncurry genLogMsg >>= hPutStrLn h
     return ()
+
+genLogMsg :: LogLevel -> String -> IO String
+genLogMsg lvl msg = do
+    timepart <- formatTime defaultTimeLocale "%F %X" <$> getZonedTime
+    return . concat $ [timepart, " ", show lvl, " ", msg]
